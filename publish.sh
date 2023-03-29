f6ba6553a50a69ddae4377fa29145fe4e49d8614
Liza Chevalier <lizalc@pm.me>
2023-03-29 19:14:17 -0400

    Delete codeql.yml

---
+++ publish.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Secret Scanning test
+# Key is fake and is for testing only. Do not use in production.
+
+dotnet nuget push Contoso.08.28.22.001.Test.1.0.0.nupkg --api-key qz2jga8pl3dvn2akksyquwcs9ygggg4exypy3bhxy6w6x6 --source https://api.nuget.org/v3/index.json
